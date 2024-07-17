3b00a7e093c763f63c93d28ca1f16b24c64d36fb
DimaDevelopment <dima.development@wandry.com.ua>
2024-07-17 15:28:09 +0300

    feat(index) Refactor departments list

--- components/departments/List.vue
+++ components/departments/List.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="departments-list">
     <DepartmentsCard
-      v-for="department in departments"
-      :name="department.name"
-      :video="department.video"
-      :link="department.link"
+      name="Фармацевтика"
+      video="/videos/pharm.mp4"
+      link="/pharm"
+    />
+    <DepartmentsCard name="Харчова" video="/videos/food.mp4" link="/food" />
+    <DepartmentsCard
+      name="Косметика"
+      video="/videos/cosm.mp4"
+      link="https://indelcosm.com.ua/"
     />
   </div>
 </template>
 
 <script>
-import orderby from "lodash.orderby";
-
 export default {
   async setup() {
     const departments = [
@@ -19,28 +22,6 @@ export default {
       { name: "Харчова", video: "/videos/food.mp4", link: "" },
       { name: "Косметика", video: "/videos/cosm.mp4", link: "" },
     ];
-
-    // const media = useStrapiMedia();
-    // const { find } = useStrapi();
-
-    // const departments = await find("napravleniyas", {
-    //   populate: "*",
-    // });
-
-    // console.log(departments);
-
-    // const departmentsForView = departments.data.map((department) => {
-    //   return {
-    //     ...department.attributes,
-    //     image: `${media}${department.attributes.image.data?.attributes.url}`,
-    //   };
-    // });
-
-    // const orderedDepartments = orderby(departmentsForView, ["order"], ["asc"]);
-
-    // return {
-    //   departments: orderedDepartments,
-    // };
 
     return {
       departments,
